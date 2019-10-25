adaf7b7c45f5c79794118e8889abc44a5cff2172
tardyparty <tardyparty@users.noreply.github.com>
2019-10-24 22:29:14 -0500

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers VALUES(NULL, "Bacon Cheeseburger", false);
+INSERT INTO burgers VALUES(NULL, "Blue Cheese Burger", false);
+INSERT INTO burgers VALUES(NULL, "Morning Glory Burger", false);
+INSERT INTO burgers VALUES(NULL, "Impossible Burger", false);
