9d4c1ca6fb9f630273a728c3c3c7ecffa52bd90c
tardyparty <tardyparty@users.noreply.github.com>
2019-11-01 23:09:43 -0500

    new router for api

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 var mysql = require('mysql');
 
 var host = process.env.DB_HOST;
+var port = process.env.DB_PORT;
 var user = process.env.DB_USER;
 var password = process.env.DB_PASSWORD;
 var database = process.env.DB_NAME;
 
 var connection = mysql.createConnection({
     host: host,
-    port: 3306,
+    port: port,
     user: user,
     password: password,
     database: database
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -39,20 +39,12 @@ router.put("/api/burgers/:id", function(req, res) {
       }
     });
   });
+
+router.get("/api/burgers", function(req, res) {
+  burger.all(function(data) {
+    res.json(data);
+  })
+})
   
-  router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    burger.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
-  
-  
-  module.exports = router;
+module.exports = router;
   
