d9acadf40d8a2eb543e0d8cd26ed18bc548ec425
tardyparty <tardyparty@users.noreply.github.com>
2019-11-03 09:36:12 -0600

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,21 @@ var user = process.env.DB_USER;
 var password = process.env.DB_PASSWORD;
 var database = process.env.DB_NAME;
 
-var connection = mysql.createConnection({
-    host: host,
-    port: port,
-    user: user,
-    password: password,
-    database: database
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+    connection = mysql.createConnection({
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        database: database
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
